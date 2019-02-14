097674b98482ba75092467c8b68823efb99d8569
Vyacheslav Linnik <vyacheslav.linnik@gmail.com>
2019-02-14 19:08:51 +0300

    update how Pyre's stderr/stdout is processed

--- bin/pyrewrapper.sh
+++ bin/pyrewrapper.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Eval given arguments and ignore STDERR output
-eval "$@" 2> /dev/null
-
 # TODO: discuss more consistent output with Pyre maintainers
-# Eval given arguments and display STDERR only on failure
-# TMP=$(mktemp)
-# eval "$@" 2> "$TMP"
-# EXITCODE=$?
-# if [ $EXITCODE -ne 0 ]; then
-#     ERR=$(cat "$TMP")
-#     (>&2 echo $ERR)
-# fi
-# rm $TMP
-# exit $EXITCODE
+# Eval given arguments and display STDERR when STDOUT is empty and exit code is non-zero
+STDERR=$(mktemp)
+STDOUT=`eval "$@" 2> $STDERR`
+EXITCODE=$?
+if [ -z "$STDOUT" ] && [ $EXITCODE -ne 0]; then
+    ERR=$(cat "$STDERR")
+    (>&2 echo $ERR)
+else
+    EXITCODE=0
+    echo $STDOUT
+fi
+rm $STDERR
+exit $EXITCODE
